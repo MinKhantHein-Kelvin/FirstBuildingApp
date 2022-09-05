5472bc2087f0460d77518b75452890824de99385
Min Khant Hein <minkhan1999ucsmub@gmail.com>
2022-09-05 11:55:33 +0630

    listingBackendv2

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,29 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
+const PORT =  4000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//connect to Database
+mongoose.connect(
+  process.env.db,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("DB is connected");
+  }
+);
+
+// import route
+const listingRouters = require("./routes/listing");
 
 
-app.listen(4000,()=>{
-  console.log('App Listing on port 4000');
-})
+//Route Middleware
+app.use(express.json());
+app.use("/api/crud", listingRouters);
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on Port at 4000");
+});
+
