70f56d8fc97261bdce4b10c8793a7cad65aec15d
Min Khant Hein <minkhan1999ucsmub@gmail.com>
2022-09-09 00:25:18 +0630

    ErrorMessagev1

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -10,10 +10,40 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   registerForm = new FormGroup({
-    name : new FormControl("", Validators.required),
-    email : new FormControl("", Validators.required),
-    password : new FormControl("", Validators.required)
-  })
+    name : new FormControl("", Validators.compose([Validators.required, Validators.maxLength(25)])),
+    email : new FormControl("", Validators.compose([Validators.required, Validators.email])),
+    password : new FormControl("", Validators.compose([Validators.required, Validators.minLength(6)])),
+    confirmPassword: new FormControl('', Validators.compose([
+      Validators.required,
+      this.passwordsMatch,
+    ])),
+  });
+
+  get Username() {
+    return this.registerForm.get('name');
+  }
+
+  get Email() {
+    return this.registerForm.get('email');
+  }
+
+  get Password() {
+    return this.registerForm.get('password');
+  }
+
+  get ConfirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  passwordsMatch(control: FormControl) {
+    const password = control.root.get('password');
+    return password && control.value != password.value
+      ? {
+          passwordMatch: true,
+        }
+      : null;
+  }
+
 
   constructor(private userService : UserService, private router : Router) { }
 
