e7097b15383a52f15702ded8eb074e23d2d8f6cf
Min Khant Hein <minkhan1999ucsmub@gmail.com>
2022-09-10 00:31:57 +0630

    lastv1

--- src/app/listing/all-listing/all-listing.component.ts
+++ src/app/listing/all-listing/all-listing.component.ts
@@ -1,7 +1,8 @@
-import { Observable } from 'rxjs';
+import { map, Observable, startWith } from 'rxjs';
 import { ListingService } from './../service/listing.service';
 import { Component, OnInit } from '@angular/core';
 import { Listing } from '../model/listing';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-all-listing',
@@ -9,13 +10,36 @@ import { Listing } from '../model/listing';
   styleUrls: ['./all-listing.component.css']
 })
 export class AllListingComponent implements OnInit {
+
+  originalData: Listing[];
   listings$ : Observable<Listing[]>
 
-  constructor(private listingService : ListingService) { }
+  obj = new FormControl('');
+  frmGroup: FormGroup;
+
+  constructor(private listingService : ListingService, private fb: FormBuilder) {
+    this.frmGroup = fb.group({
+      obj: [],
+    });
+  }
 
   ngOnInit(): void {
-    this.listings$ = this.listingService.getListings();
-    // console.log(this.listings$);
+    // this.listings$ = this.listingService.getListings();
+    this.listingService.getListings().subscribe((data: Listing[]) => {
+      this.originalData = data;
+      if (this.originalData && this.originalData.length > 0) {
+        this.listings$ = this.frmGroup.get('obj')!.valueChanges.pipe(
+          startWith(''),
+          map((text) => this.search(text, this.originalData))
+        );
+      }
+    });
+  }
+  search(text: string, listings: Listing[]): any {
+    return listings.filter((listing: Listing) => {
+      const term = text.toLowerCase();
+      return listing.title && listing.title.toLowerCase().includes(term);
+    });
 
   }
 
